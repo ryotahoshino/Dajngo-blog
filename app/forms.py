27e33a26b9f378e3931925cf234d8d0f875c00d4
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2022-01-23 17:38:04 +0900

    フォームの作成

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from cProfile import label
+from socket import fromshare
+from django import forms
+
+class PostForm(forms.Form):
+  title = forms.CharField(max_length=30, label='タイトル')
+  content = forms.CharField(label='内容', widget=forms.Textarea())
